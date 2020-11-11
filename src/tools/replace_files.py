481815e8e9455a62d06574ac1da781e36b8057c8
Lucas Sievers <lucas.sievers@gmail.com>
2020-11-11 09:26:29 -0300

    Initialization Files

--- src/tools/replace_files.py
+++ src/tools/replace_files.py
@@ -5,10 +5,10 @@ from distutils import dir_util
 
 def replace_files():
     local_base_path = os.path.dirname(os.path.realpath(__file__))
-    local_path = f'{local_base_path}{os.sep}files_to_replace'
-    dofus_theme_path = f"{os.getenv('LOCALAPPDATA')}{os.sep}Ankama{os.sep}zaap{os.sep}dofus"
-    ui_positions_log = f"{os.getenv('APPDATA')}{os.sep}Dofus{os.sep}Berilia_ui_positions.dat"
+    local_path_local = f'{local_base_path}{os.sep}files_to_replace{os.sep}local'
+    local_path_roaming = f'{local_base_path}{os.sep}files_to_replace{os.sep}roaming'
+    dofus_local_path = f"{os.getenv('LOCALAPPDATA')}{os.sep}Ankama{os.sep}zaap{os.sep}dofus"
+    dofus_roaming_path = f"{os.getenv('APPDATA')}"
+    distutils.dir_util.copy_tree(local_path_local,dofus_local_path)
+    distutils.dir_util.copy_tree(local_path_roaming,dofus_roaming_path)
 
-    distutils.dir_util.copy_tree(local_path,dofus_theme_path)
-    if os.path.exists(ui_positions_log):
-        os.remove(ui_positions_log)
